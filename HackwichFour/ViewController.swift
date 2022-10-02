53e1196455ced06fd0a9bbb6473fd59a5a654988
Kaylene <carnavos@hawaii.edu>
2022-10-02 12:09:14 -1000

    Completed parts 1-8

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTabLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.firstTabLabel.text = "About"
+    
     }
 
 
---
+++ secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  HackwichFour
+//
+//  Created by Kaylene Carnavos on 10/2/22.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var secondTabLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
